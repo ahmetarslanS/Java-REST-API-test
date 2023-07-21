a8d7f45035044ddd8f2ad65d521a481e121dc4e2
ahmetarslanS <ahmet_1285_arslan@ghotmail.com>
2023-07-21 16:34:05 +0300

    test

--- src/main/java/com/beginsecure/messenger/MessengerAppApplication.java
+++ src/main/java/com/beginsecure/messenger/MessengerAppApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MessengerAppApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(MessengerAppApplication.class, args);
 	}
 
