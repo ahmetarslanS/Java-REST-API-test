50a6a247b262a86e910481e195781dc8d7a5c99a
ahmetarslanS <ahmet_1285_arslan@ghotmail.com>
2023-07-31 11:44:34 +0300

    Swagger-UI added

--- src/main/java/com/beginsecure/messenger/MessengerAppApplication.java
+++ src/main/java/com/beginsecure/messenger/MessengerAppApplication.java
@@ -2,13 +2,13 @@ package com.beginsecure.messenger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.beginsecure.messenger") // Add this line
 public class MessengerAppApplication {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(MessengerAppApplication.class, args);
 	}
-
 }
--- src/main/java/com/beginsecure/messenger/controller/MessageController.java
+++ src/main/java/com/beginsecure/messenger/controller/MessageController.java
@@ -1,20 +1,39 @@
 package com.beginsecure.messenger.controller;
 
 import com.beginsecure.messenger.domain.Message;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Api(tags = "Message API", description = "Endpoints for sending messages")
 public class MessageController {
 
+    @ApiOperation(value = "Get a hello message", notes = "Returns a hello message with the provided name.")
     @GetMapping("/hello")
-    public Message startMessage(@RequestParam(value = "name", defaultValue = "World") String name){
-        return new Message(String.format("Hello %s!" , name));
+    public Message startMessage(@ApiParam(value = "Name to be used in the hello message", defaultValue = "World")
+                                @RequestParam(value = "name", defaultValue = "World") String name) {
+        return new Message(String.format("Hello %s!", name));
     }
 
+    @ApiOperation(value = "Get a goodbye message", notes = "Returns a goodbye message with the provided name.")
     @GetMapping("/goodbye")
-    public Message endMessage(@RequestParam(value = "name" , defaultValue = "World") String name){
-        return new Message(String.format("See you next time %s!" , name));
+    public Message endMessage(@ApiParam(value = "Name to be used in the goodbye message", defaultValue = "World")
+                              @RequestParam(value = "name", defaultValue = "World") String name) {
+        return new Message(String.format("See you next time %s!", name));
+    }
+
+    // Exception handler for handling Not Found (404) errors
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ResponseEntity<String> handleNotFoundException() {
+        return new ResponseEntity<>("Error: Resource not found", HttpStatus.NOT_FOUND);
     }
 }
