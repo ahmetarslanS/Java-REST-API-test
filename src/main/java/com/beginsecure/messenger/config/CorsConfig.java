2f15716be0fee06d75a018fd623ddfeb9cdd1c8e
ahmetarslanS <ahmet_1285_arslan@ghotmail.com>
2023-08-01 10:33:29 +0300

    CORS Config added.

---
+++ src/main/java/com/beginsecure/messenger/config/CorsConfig.java
@@ -0,0 +1,19 @@
+package com.beginsecure.messenger.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Allow all endpoints to have CORS enabled
+                .allowedOrigins("*") // Replace "*" with specific origins if needed
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("Content-Type")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
