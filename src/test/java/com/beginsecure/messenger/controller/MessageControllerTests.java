e26dc377659db6c5ad57e8da7e378db8b5e1f3bb
ahmetarslanS <ahmet_1285_arslan@ghotmail.com>
2023-08-03 13:46:50 +0300

    unit tests

---
+++ src/test/java/com/beginsecure/messenger/controller/MessageControllerTests.java
@@ -0,0 +1,35 @@
+package com.beginsecure.messenger.controller;
+
+import com.beginsecure.messenger.domain.Message;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class MessageControllerTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testHelloEndpoint() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/hello").param("name", "John")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content").value("Hello John!"));
+    }
+
+    @Test
+    public void testGoodbyeEndpoint() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/goodbye").param("name", "Jane")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content").value("See you next time Jane!"));
+    }
+}
---
+++ src/test/java/com/beginsecure/messenger/controller/SpringDocumentationTests.java
@@ -0,0 +1,26 @@
+package com.beginsecure.messenger.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class SpringDocumentationTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testSwaggerDocumentation() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/v2/api-docs")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.paths").exists());
+    }
+}
